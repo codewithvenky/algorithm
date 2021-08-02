ba99fa87dbfefc6c74ba7088553133182d3395e5
codewithvenky <codewithvenky@gmail.com>
2021-08-02 22:24:41 +0530

    adding  Maven Project Support

---
+++ src/main/java/com/venky/FibonacciRecursion.java
@@ -0,0 +1,29 @@
+package com.venky;
+
+public class FibonacciRecursion {
+
+    static int  n1 =0,n2 =1,n3=0;
+
+    public static void main(String[] args) {
+       int  count=10;
+        System.out.print(n1 + "," +n2);
+
+        fibonacci(count-2);
+
+    }
+
+    private static void fibonacci(int count) {
+        if(count>0)
+        {
+            n3= n1+n2;
+            n1=n2;
+            n2=n3;
+            System.out.print("," +n3);
+           fibonacci(count-1);
+        }
+
+
+    }
+
+
+}
